85a026bc6a5b84b16875a361ea1eba29619901ec
Rafael Vicente <rafaelpv15@gmail.com>
2021-03-05 10:36:47 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
-
+app.use(cors());
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
