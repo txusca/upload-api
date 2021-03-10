b766c7c0147dc181b13eb8944ffddb0d25e83e2b
Rafael Vicente <rafaelpv15@gmail.com>
2021-03-10 16:28:53 -0300

    Remove get in users

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,13 +2,7 @@ const router = require('express').Router();
 
 const User = require('../models/User');
 
-router.get('/', async (req, res) => {
-    const users = await User.find().lean();
-
-    return res.json(users);
-});
-
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
     try {
         const user = await User.create(req.body);
         return res.json(user);
